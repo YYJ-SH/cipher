435dec18261c060c75bee985a45a6f2673a50dd0
YYJ-SH <20200936@sunshin.ac.kr>
2025-05-12 17:02:32 +0900

    foint changed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,6 @@
-import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { Inter } from "next/font/google";
 
 export const metadata = {
   title: "시저암호 해독기",
@@ -19,11 +10,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className="antialiased">
+      {children}
       </body>
     </html>
-  );
+    );
 }
